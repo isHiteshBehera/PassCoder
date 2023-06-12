aaa11c1775b5f784cb55826234db547a2d471541
isHiteshBehera <bhiteshme74@gmail.com>
2023-06-12 21:23:23 +0530

    foundation layed

--- app/src/main/java/com/example/passcoder/MainActivity.java
+++ app/src/main/java/com/example/passcoder/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.passcoder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText f = (EditText) findViewById(R.id.editTextText);
+                TextView res = (TextView) findViewById(R.id.displayResult);
+
+                int range = Integer.parseInt(f.getText().toString());
+                Random r = new Random();
+                int rand_num = r.nextInt(range - 0) + 0;
+                res.setText(rand_num + " ");
+
+            }
+        });
+
+
+
     }
 }
